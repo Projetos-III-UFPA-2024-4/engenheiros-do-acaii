c13fb4394d7f3cc78f4864b545fba3ad5e5ddd73
davidpsousa <davidpinheirosousa.ds@gmail.com>
2025-02-21 20:18:14 -0300

    Alteracao no formato de salvamento de data e hora

--- backend/servicos/crud-dados/models/MedidorModel.js
+++ backend/servicos/crud-dados/models/MedidorModel.js
@@ -8,14 +8,11 @@ export const inserirDados = async (dados) => {
     try {
         for (const dado of dados) {
             // ✅ Converte os valores de hora, minuto e segundo para um formato DATETIME (YYYY-MM-DD HH:MM:SS)
-            const dataHoraFormatada = new Date();
-            dataHoraFormatada.setHours(dado.hora);
-            dataHoraFormatada.setMinutes(dado.minuto);
-            dataHoraFormatada.setSeconds(dado.segundo);
+            const dataHoraFormatada = `${new Date().toISOString().slice(0, 10)} ${dado.hora}:${dado.minuto}:${dado.segundo}`;
 
             // ✅ Mapeia os nomes antigos do JSON para os novos nomes no banco
             const valores = [
-                11, dataHoraFormatada.toISOString().slice(0, 19).replace('T', ' '), // Formata para DATETIME
+                11, dataHoraFormatada, // Formata para DATETIME
                 dado.pa, dado.pb, dado.pc, dado.pt,   // potA, potB, potC, potTotal
                 dado.epa_c, dado.epb_c, dado.epc_c, dado.ept_c, // consumoA, consumoB, consumoC, consumoTotal
                 dado.epa_g, dado.epb_g, dado.epc_g, dado.ept_g, // geracaoA, geracaoB, geracaoC, geracaoTotal
